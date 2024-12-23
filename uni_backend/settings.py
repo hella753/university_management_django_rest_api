4a85d014cc642b1add44f9a324042375da849665
hella753 <kristi.gaphrindashvili@gmail.com>
2024-12-23 21:23:40 +0400

    removes session auth from settings

--- uni_backend/settings.py
+++ uni_backend/settings.py
@@ -171,7 +171,6 @@ MEDIA_ROOT = BASE_DIR / 'media/'
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework_simplejwt.authentication.JWTAuthentication',
-        'rest_framework.authentication.SessionAuthentication',
     ],
     'DEFAULT_FILTER_BACKENDS': [
         'django_filters.rest_framework.DjangoFilterBackend',
